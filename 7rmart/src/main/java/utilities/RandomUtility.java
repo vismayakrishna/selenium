00a4c5e5d6b50828a05e7e4f8959fa7cb4c0d31b
vismayakrishna <vismayaradhakrishnan97@gmail.com>
2024-02-01 17:47:53 +0530

    February - 1 commit

--- 7rmart/src/main/java/utilities/RandomUtility.java
+++ 7rmart/src/main/java/utilities/RandomUtility.java
@@ -1,5 +1,33 @@
 package utilities;
 
+import com.github.javafaker.Faker;
+
 public class RandomUtility {
 
+	Faker faker;
+	
+	public String generateFirstName() {
+		faker = new Faker();
+		return faker.name().firstName();
+	}
+	
+	public String generateLastName() {
+		faker = new Faker();
+		return faker.name().lastName();
+	}
+	
+	public String generatePhoneNumber() {
+		faker = new Faker();
+		return faker.phoneNumber().phoneNumber();
+	}
+	
+	public String generateEmail() {
+		faker = new Faker();
+		return faker.internet().emailAddress();
+	}
+	
+	public String generatePassword(int minLength, int maxLength) {
+		faker = new Faker();
+		return faker.internet().password(minLength, maxLength, true, true, true);
+	}
 }
