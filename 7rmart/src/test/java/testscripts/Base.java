156ce6a7683a6fda761b13e73a94a8995ec579fd
Vismaya <vismayaradhakrishnan97@gmail.com>
2024-02-06 11:07:25 +0530

    February 6 - commit

--- 7rmart/src/test/java/testscripts/Base.java
+++ 7rmart/src/test/java/testscripts/Base.java
@@ -4,16 +4,28 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class Base {
 	
-public WebDriver driver;
+	public WebDriver driver;
 	
 	@BeforeMethod
-	public void initializeBrowser() {
-		driver = new ChromeDriver();
+	@Parameters("browser")
+	public void initializeBrowser(String browser) throws Exception {
+		if(browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if(browser.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		} else if(browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else {
+			throw new Exception("Invalid browser");
+		}
 		driver.get("https://groceryapp.uniqassosiates.com/admin ");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(70));
 		driver.manage().window().maximize();
